2fddddd597e95806589e3890fd232d4b46d843f0
Yongho Lee <ylee01@stuy.edu>
2018-12-13 11:34:14 -0500

    Added MyLinkedList.java

---
+++ MyLinkedList.java
@@ -0,0 +1,38 @@
+public class MyLinkedList {
+  private class MyNode {
+    private Integer data;
+    private Node next, prev;
+    public Node next() {
+      return next;
+    }
+    public Node prev() {
+      return prev;
+    }
+    public void setNext(Node other) {
+      next = other;
+    }
+    public void setPrev(Node other) {
+      prev = other;
+    }
+    public Integer getData() {
+      return data;
+    }
+    public Integer setData(Integer i) {
+
+    }
+    public String toString(){
+
+    }
+  }
+
+  private int size;
+  private Node start,end;
+  public int size() {
+  }
+  public boolean add(int value) {
+
+  }
+  public String toString() {
+
+  }
+}
